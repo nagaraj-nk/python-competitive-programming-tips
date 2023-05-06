4ee5f8f8fc5b6d41c58bf19357635c14c660ac54
Nagaraj N <nagaraj.srjons@gmail.com>
2023-05-06 19:43:06 +0530

    new tricks added

---
+++ most_common.py
@@ -0,0 +1,8 @@
+# Code to find top 3 elements and their counts
+# using most_common
+from collections import Counter
+
+arr = [1, 3, 4, 1, 2, 1, 1, 3, 4, 3, 5, 1, 2, 5, 3, 4, 5]
+counter = Counter(arr)
+top_three = counter.most_common(3)
+print(top_three)
